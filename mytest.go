b585964ea3e5b98c57cd8c5b44ed2675d8f24815
buptWYChen <32572844+buptWYChen@users.noreply.github.com>
2023-11-02 14:22:42 +0800

    Update mytest.go

--- mytest.go
+++ mytest.go
@@ -1,8 +1,8 @@
-package test
+package importtest
 
 import "fmt"
 
-// TestPrintln 打印字符串，前加上"测试输出:"字样
-func TestPrintln(str string) {
-	fmt.Println("测试输出:" + str)
+// TestPrintln2 打印字符串，前加上"测试输出:"字样
+func TestPrintln2(str string) {
+	fmt.Println("测试输出2:" + str)
 }
